cedd787c23c517e3cad331459aec2c1b71f42305
shristi2108 <shristi21aug@gmail.com>
2018-01-23 21:58:30 +0530

    first commit

---
+++ maven-test/src/test/java/com/test/maven/demoTest.java
@@ -0,0 +1,14 @@
+package com.test.maven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class demoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
